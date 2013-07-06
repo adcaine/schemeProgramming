02262d32fded7f892e83b3e5bed26da3a7f6e98f
Allan Caine <adcaine@gmail.com>
2013-07-06 19:10:43 -0400

    Add Substitutions by Index

---
+++ substituting.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname substituting) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
+(define (atIndex idx lst)
+  (cond
+    [(or (empty? lst) (< idx 1)) empty]
+    [(= 1 idx) (first lst)]
+    [else (atIndex (sub1 idx) (rest lst))]))
+
+(define (substitute indicesList atomsList)
+  (cond
+    [(or (empty? indicesList) (empty? atomsList)) empty]
+    [else (cons (atIndex (first indicesList) atomsList) (substitute (rest indicesList) atomsList))]))
+
+(substitute (list 4 4 2 1) (list 'a 'b 'c 'd))
+
+(substitute empty (list 'a 'b 'c 'd))
+
+(substitute (list 2 2 1) (list 'a 'b 'c 'd))
+
+(substitute (list 4 4 2 1) empty)
